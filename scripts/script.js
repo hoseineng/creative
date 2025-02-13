0a162a3eaa273d1c2f5d29eff271f91fa43f52fe
hoseinparinoor <hoseinparinoor@gmail.com>
2025-02-13 15:13:17 +0330

    comment code js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-const headerToggleIcon = document.querySelector('.header__toggle-icon')
-const mobileMenu = document.querySelector('.mobile-menu')
+// const headerToggleIcon = document.querySelector('.header__toggle-icon')
+// const mobileMenu = document.querySelector('.mobile-menu')
 
-headerToggleIcon.addEventListener('click', function () {
-    this.classList.toggle('header__toggle-icon--open')
-    mobileMenu.classList.toggle('mobile-menu--open')
-})
+// headerToggleIcon.addEventListener('click', function () {
+//     this.classList.toggle('header__toggle-icon--open')
+//     mobileMenu.classList.toggle('mobile-menu--open')
+// })
